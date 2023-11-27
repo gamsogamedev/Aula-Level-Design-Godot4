2744dfdd33993b868e7fa4a7c40f6dddeae6851f
Yamartim <54243954+Yamartim@users.noreply.github.com>
2023-11-27 18:01:11 -0300

    add camera e aumentei cena

---
+++ PlayerCamera2D.gd
@@ -0,0 +1,8 @@
+extends Camera2D
+
+@onready var character_body_2d = $".."
+@export var lerp_speed: float = 1.0
+
+func _process(delta: float) -> void:
+    pass
+    #offset = offset.lerp(character_body_2d.velocity, delta * lerp_speed)
